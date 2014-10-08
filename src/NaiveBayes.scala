65a574755cda1ded6dbe47daac95477981879383
hambone20 <hambone20@gmail.com>
2014-10-08 13:51:45 -0700

    refactored some code

--- src/NaiveBayes.scala
+++ src/NaiveBayes.scala
@@ -1,5 +1,5 @@
 package naivebayes
-import Math.log
+//import Math.log
 import scala.collection.mutable
 
 /** Workhorse of our Bayesian classifier
@@ -12,8 +12,7 @@ import scala.collection.mutable
  */
 
 class NaiveBayes(trainingSet: Array[Array[Int]], classes: Array[Int], binning: Boolean = false) {
-  // Mapping will contain P on class 
-  
+  type Probability = Double
   val trainingSize    = trainingSet.size
   val numAttributes   = trainingSet.head.size - 1 // the last element is the actual classification
   val attributeValues = if(!binning) Range(0, 17).toArray
@@ -23,7 +22,7 @@ class NaiveBayes(trainingSet: Array[Array[Int]], classes: Array[Int], binning: B
   val numValues = attributeValues.size
   assert((binning && numValues == 4) || numValues == 17, "Error in sizing attributes")
   
-  var model = Map[String, Double]() // hold our model
+  var model = Map[String, Probability]() // hold our model
   
   // A. compute prior probability for each class ie. P('1')
   computePForClasses()
@@ -31,80 +30,93 @@ class NaiveBayes(trainingSet: Array[Array[Int]], classes: Array[Int], binning: B
   // ie. P(Attribute1=0|'1') is the probability of that Attribute 1 has value 0, given that the class is digit '1' 
   // C. smooth the conditional probabilities using Laplace (add-one) smoothing
   for(cls <- classes){
-    	computePForEachAttribute(cls) // calc for given class
+    	computePForEachAttribute(cls) // calculate for all classes
   }
   /** computes prior probability for all classes
    * 
    * i.e. P('1')
    */
-  def computePForClasses() {
+  private def computePForClasses() {
     for(cls <- classes){
-      val count = trainingSet.filter(_.last == cls).size
-      val P = count.toDouble / trainingSize
+      val count = trainingSet.filter(x => x.last == cls).size
+      val P: Probability = count.toDouble / trainingSize
       model += Tuple2(cls.toString, P)
     }
+    assert(model.size == classes.size, "Error creating model for classes")
   }
   
-  /** computes prior probability for each attribute
+  /** computes prior probability for each attribute, adding Laplace smoothing
    * 
-   * i.e. P(Attribute1=0|'1')
+   * i.e. P(Attribute1=0|'1') is inserted into the model with the key: 1=0|1
    * @param cls class to compute
    */
-  def computePForEachAttribute(cls: Int) { // adjust for Laplace smoothing
+  private def computePForEachAttribute(cls: Int) {
     val clsData = trainingSet.filter(x => x.last == cls)
-    // attributes 0-16, numValues = 17 of them
+    
     for(attIdx <- 0 to numAttributes){
-      for(value <- attributeValues){ // we have the form P(att{attIdx}={value}|cls)
-        val key: String = if(!binning) s"$attIdx=$value|$cls"
-        		          else s"$attIdx=${(value.asInstanceOf[Array[Int]]).mkString}|$cls" 
-        val attValData = if(!binning) clsData.filter(x => x(attIdx) == value) // data that has the given attribute value
-                         else{
-                        	 clsData.filter(x => value.asInstanceOf[Array[Int]].contains(x(attIdx)))
-                         }
+      for(value <- attributeValues){   
+        val (key, attValData) = value match {
+          case idx: Int => (s"$attIdx=$idx|$cls", clsData.filter(x => x(attIdx) == value))
+          case arr: Array[Int] => (s"$attIdx=${arr.mkString}|$cls", clsData.filter(x => arr.contains(x(attIdx))))
+          case _ => throw new Exception("Attributes were improperly formed")
+        }
                  
         model += Tuple2(key, (attValData.size + 1.0) / (clsData.size + numValues))
       }    
     }
   }
+  /* type aliases for classification */
+  type ActualClass = Int
+  type ConfidenceLevel = Double
+  type ClassifiedClass = Int
   
   /** classify based on the training data given
    * 
+   * calculate confidence interval for all classes, then find class that maximizes
    * @param testSet Data set to classify given the model
-   * @return array of classifications 
+   * @return array of classifications where each element is a Tuple of (ActualClass, Confidence, Classification)
    */
-  def classify(testSet: Array[Array[Int]]) = {
-    // calculate confidence interval for all classes, find class that maximizes
-    var result = mutable.ArrayBuffer[(Int, Double, Int)]()
+  def classify(testSet: Array[Array[Int]]): Array[(ActualClass, ConfidenceLevel, ClassifiedClass)] = {
+    var result = Array[(ActualClass, ConfidenceLevel, ClassifiedClass)]()
     for(test <- testSet){
-	    var maxClass = (0, 0.0) // class, confidence
-	    var initMax = false
-	    val actualClass = test.last
+	    val actualClass: ActualClass = test.last
+	    var maxClass: (ClassifiedClass, ConfidenceLevel) = (0, 0.0)
+	    var maxInit = false
+	    
 	    for(cls <- classes){
-	      var confidence = log(model.getOrElse(cls.toString, throw new Exception("shouldn't reach"))) // log P(class)
+	      var confidence: ConfidenceLevel = Math.log(model.getOrElse(cls.toString, throw new Exception("shouldn't reach"))) // log P(class)
 	      for(attIdx <- 0 to numAttributes){
 	        //form value
-	        val vall: String = if(!binning) test(attIdx).toString
-	                   else {
-	                     def findAttribute() :String = {
-	                    	 for(i <- 0 until attributeValues.asInstanceOf[Array[Array[Int]]].size){
-	                           if(attributeValues.asInstanceOf[Array[Array[Int]]](i).contains(test(attIdx))){
-	                             return attributeValues.asInstanceOf[Array[Array[Int]]](i).mkString 
-	                           }
-	                         }
-	                    	 throw new Exception("shouldn't reach")
-	                     }
-	                     findAttribute()
+	        val vall: String = if(!binning) test(attIdx).toString //TODO refactor into a match
+	                   else {       
+	                     findAttribute(test, attIdx)
 	                   }
-	        confidence += log(model.getOrElse(s"$attIdx=${vall}|$cls", throw new Exception("shouldn't reach"))) // + log(P(x_n|class)
+	        val key = s"$attIdx=${vall}|$cls"
+	        confidence += Math.log(model.getOrElse(key, throw new Exception("shouldn't reach"))) // + log(P(x_n|class)
 	      }
-	      // confidence calced for given class
-	      if(confidence > maxClass._2 || !initMax){ // found new prospective class
+	      // confidence calculated for given class
+	      if(confidence > maxClass._2 || !maxInit){ // found new prospective class
 	        maxClass = Tuple2(cls, confidence)
-	        initMax = true
+	        maxInit = true
 	      }
 	    }
-	    result += Tuple3(actualClass, maxClass._2, maxClass._1)
+	    val el: (ActualClass, ConfidenceLevel, ClassifiedClass) = Tuple3(actualClass, maxClass._2, maxClass._1)
+	    result = result :+ el
     }
     result
+  }
+  
+  /** A helper method for classify
+   * 
+   * Finds attribute when binning is done
+   * @return 
+   */
+  private def findAttribute(test: Array[Int], attIdx: Int): String = {
+    for (i <- 0 until attributeValues.asInstanceOf[Array[Array[Int]]].size) {
+      if (attributeValues.asInstanceOf[Array[Array[Int]]](i).contains(test(attIdx))) {
+        return attributeValues.asInstanceOf[Array[Array[Int]]](i).mkString
+      }
+    }
+    throw new Exception("shouldn't reach")
   }
 }
